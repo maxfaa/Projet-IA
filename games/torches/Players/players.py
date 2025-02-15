df22807b34d0694704e9fe26e9d699c6a1801c25
Hug00at <etu52503@henallux.be>
2025-02-15 01:35:50 +0100

    Update players.py
    type hinting players

--- games/torches/Players/players.py
+++ games/torches/Players/players.py
@@ -2,26 +2,26 @@ import random
 
 # --- Player Classes ---
 class Player:
-    def __init__(self, name, game=None):
+    def __init__(self, name : str, game=None) -> None:
         self.name = name
         self.game = game
         self.wins = 0
         self.losses = 0
 
     @property
-    def nb_games(self):
+    def nb_games(self) -> int:
         return self.wins + self.losses
 
     @staticmethod
-    def play():
+    def play() -> int:
         return random.randint(1, 3)
 
-    def win(self):
+    def win(self) -> None:
         self.wins += 1
 
-    def lose(self):
+    def lose(self) -> None:
         self.losses += 1
 
 class Human(Player):
-    def play(self):
+    def play(self) -> int:
         return int(input(f"{self.name}, combien d'allumettes prenez-vous (1-3) ? "))
